b8b2814015bc52039e663bc421c288ea0589c686
k-kyg <53766921+k-kyg@users.noreply.github.com>
2021-06-08 22:01:21 +0900

    テーマカラーの変更

--- ts/style.ts
+++ ts/style.ts
@@ -1,10 +1,14 @@
 import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCRipple } from '@material/ripple';
+import { MDCTextField } from '@material/textfield';
 
-// Instantiation
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement!);
 const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
 const ripples = [].map.call(document.querySelectorAll(selector), function (el) {
 	return new MDCRipple(el);
 });
+
+
+const columnField = new MDCTextField(document.querySelector('.columnField')!);
+const rowField = new MDCTextField(document.querySelector('.rowField')!);
